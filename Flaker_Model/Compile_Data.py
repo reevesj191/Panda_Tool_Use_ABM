43387f6a817350984f382b6642175ead7d1159b4
reevesj191 <jonathanscott.reeves@gmail.com>
2022-04-12 11:23:50 +0200

    flaker_model

---
+++ Flaker_Model/Compile_Data.py
@@ -0,0 +1,94 @@
+import os
+from abm_functions import create_DB, select_table, add_run_data, add_source_data,\
+    add_tree_data, add_tool_data, add_env_data, connect_db
+
+pathway = "Param_Sweep_iterations"
+db_name = "0000_Run_Compiled"
+db_path = os.path.join(pathway,db_name)
+
+
+## Create Master Data Base
+if not os.path.exists(db_path):
+    create_DB(db_path)
+
+runs = os.listdir(pathway)
+runs.remove(db_name)
+runs = [run for run in runs if not run.endswith("-journal")]
+#runs.remove('.DS_Store')
+
+
+master_conn = connect_db(db_path)
+c = master_conn.cursor()
+sql_exp = """ALTER TABLE run_data ADD n_trees_available_start integer """
+c.execute(sql_exp)
+sql_exp = """ALTER TABLE run_data ADD n_trees_available_end integer """
+c.execute(sql_exp)
+
+
+for run in runs:
+    print(run)
+    run_path = os.path.join(pathway, run)
+    run_conn = connect_db(run_path)
+
+    ### compile run_data
+
+    run_data = select_table(run_conn, "run_data")
+
+    # Add two columns
+    if len(run_data) > 0:
+
+        ## Add Tree_data
+        print("adding trees")
+        trees = select_table(run_conn, "trees")
+        for tree in trees:
+            add_tree_data(master_conn, tree, commit_now=False)
+    #     ### Add Source_data
+
+        print("adding sources")
+        sources = select_table(run_conn, "sources")
+        for source in sources:
+            add_source_data(master_conn, source, commit_now=False)
+    #     ### Add tool_data
+
+        print("adding Tools")    
+        tools = select_table(run_conn, "tools")
+        for tool in tools:
+            add_tool_data(master_conn, tool, commit_now=False)
+
+    #     ### Environment Data
+        
+        print("adding environment")
+        env = select_table(run_conn, "environment")
+        for row in env:
+            add_env_data(master_conn, row, commit_now=False)
+
+
+        start = env[1]
+        start = start[1]
+
+        end = env[len(env)-1]
+        end = end[1]
+
+        add_run_data(master_conn, run_data[0])
+
+        sql_exp = """ 
+        UPDATE run_data 
+        SET n_trees_available_start = {start}
+        WHERE run_id = '{run_id}'
+        """.format(start=start, run_id=run)
+
+        c.execute(sql_exp)
+
+        sql_exp = """ 
+        UPDATE run_data 
+        SET n_trees_available_end = {start}
+        WHERE run_id = '{run_id}'
+        """.format(start=end, run_id=run)
+
+        c.execute(sql_exp)
+
+
+    else:
+        print("no data to add")
+
+    master_conn.commit()
---
+++ Flaker_Model/behavior_space.py
@@ -0,0 +1,32 @@
+from Model_definition import *
+from mesa.batchrunner import BatchRunnerMP
+from multiprocessing import freeze_support
+
+
+
+# PARAMTERS THAT WILL REMAIN THE SAME IN ALL RUNS
+
+fixed_params = {"width": 250,
+                "height":  250,
+                "Na": 100,
+                "max_ts": 75000,
+                "db_name": "Param_Sweep"
+}
+
+# PARAMETERS TO BE VARIED
+
+variable_params = {
+    "Nn": (100,500,1000,2000),
+     "Ns": (10,100,500),
+    "treesdie":(True, False),
+}
+
+if __name__ == '__main__':
+    freeze_support()
+
+    mp_batch_run = BatchRunnerMP(model_cls=PrimToolModel,
+                             nr_processes=4, # Number of cores to use
+                             variable_parameters=variable_params, # See line 18 
+                             fixed_parameters=fixed_params, # See line 9
+                             iterations=2, # The number of interations for each combination of parameters
+                             max_steps=750001) # Make sure this number if greater than "max_ts" on line 12
